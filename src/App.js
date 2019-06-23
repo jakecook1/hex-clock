538a9ed2862668877d7589e34a890e6b810eb4ad
jakecook1 <jake.cook1@gmail.com>
2019-06-23 15:03:51 +1200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
+import Moment from 'moment';
+import { Helmet } from "react-helmet";
+import { Container } from 'react-bootstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+
+        const currentDateTime = Moment();
+
+        this.state = {
+            hexTime: currentDateTime.format("HHmmss"),
+            time: currentDateTime.format("HH:mm:ss")
+        };
+
+        this.tick = this.tick.bind(this);
+    }
+
+    tick() {
+        const currentDateTime = Moment();
+        this.setState({
+            hexTime: currentDateTime.format("HHmmss"),
+            time: currentDateTime.format("HH:mm:ss")
+        });
+    }
+
+    componentDidMount() {
+        const currentDateTime = Moment();
+        this.setState({
+            hexTime: currentDateTime.format("HHmmss"),
+            time: currentDateTime.format("HH:mm:ss")
+        });
+
+        this.interval = setInterval(this.tick, 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    hexToLuma = (colour) => {
+        const hex = colour.replace(/#/, '');
+        const r = parseInt(hex.substr(0, 2), 16);
+        const g = parseInt(hex.substr(2, 2), 16);
+        const b = parseInt(hex.substr(4, 2), 16);
+    
+        let luma = [0.299 * r, 0.587 * g, 0.114 * b].reduce((a, b) => a + b) / 255;
+        return luma > 0.5 ? 'black' : 'white';
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <Helmet bodyAttributes={{style: 'background-color : #' + this.state.hexTime}}/>
+                <Container className="display-1 text-monospace text-center mt-3">
+                    <p style={{ color: this.hexToLuma(this.state.hexTime) }}>
+                        {this.state.time}
+                    </p>
+                </Container>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
